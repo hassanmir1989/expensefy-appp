9ce8de50d928828daf327ec1adae3a884626b1d4
hassan mir <hassanmir@DESKTOP-A52A746.COM>
2018-09-15 23:44:02 +0300

    qw12

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const pathName = path.join(__dirname,"..","public")
-
+const port = process.env.PORT || 3000
 
 app.use(express.static(pathName))
 app.engine("html",require("ejs").renderFile)
@@ -15,6 +15,6 @@ app.get("*",function(req,res){
 })
 
 
-app.listen(process.env.PORT || 3000,() => {
+app.listen(port,() => {
     console.log("server initiated")
 })
